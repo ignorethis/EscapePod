0e73d18d651ae65293166cfb1fb648402b6186b0
egi <daegi01@gmail.com>
2020-01-08 02:10:32 +0100

    remove double dots in file names.

--- EscapePod/PodcastService.cs
+++ EscapePod/PodcastService.cs
@@ -107,14 +107,14 @@ namespace Pp
         {
             return mimeType switch
             {
-                "audio/mpeg" => ".mp3",
-                "audio/x-m4a" => ".m4a",
-                "audio/x-wav" => ".wav",
-                "audio/x-aiff" => ".aif",
-                "audio/x-pn-realaudio" => ".ra",
-                "audio/x-ms-wma" => ".wma",
-                "audio/midi" => ".mid",
-                _ => ".mp3"
+                "audio/mpeg" => "mp3",
+                "audio/x-m4a" => "m4a",
+                "audio/x-wav" => "wav",
+                "audio/x-aiff" => "aif",
+                "audio/x-pn-realaudio" => "ra",
+                "audio/x-ms-wma" => "wma",
+                "audio/midi" => "mid",
+                _ => "mp3"
             };
         }
 
