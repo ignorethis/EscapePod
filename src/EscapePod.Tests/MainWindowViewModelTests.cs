64db0a2a7e9e042e104d740dfa59f6a636e13070
ignorethis <6a.70.74.68.65.31@googlemail.com>
2024-10-31 22:41:46 +0100

    Added unit test project

---
+++ src/EscapePod.Tests/MainWindowViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using EscapePod.ViewModels;
+using FluentAssertions;
+using Xunit;
+
+namespace EscapePod.Tests
+{
+    public class MainWindowViewModelTests
+    {
+        [Fact]
+        public void ConstructorTest1() 
+        { 
+            MainWindowViewModel viewModel = new MainWindowViewModel();
+            viewModel.Should().NotBeNull();
+        }
+    }
+}
