524deae5c86b309cc07c32326c76edaf4cabb599
egi <daegi01@gmail.com>
2025-07-27 04:15:46 +0200

    Save on OnShutdownRequested and cleanup

--- src/EscapePod/App.axaml.cs
+++ src/EscapePod/App.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -7,8 +8,11 @@ using EscapePod.Views;
 
 namespace EscapePod;
 
-public partial class App : Application
+public class App : Application
 {
+    private PodcastService? _podcastService;
+    private MainWindowViewModel? _mainWindowViewModel;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -18,19 +22,44 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
+            _podcastService = new PodcastService();
+            _mainWindowViewModel = new MainWindowViewModel(_podcastService);
+
+            desktop.ShutdownRequested += OnShutdownRequested;
             desktop.MainWindow = new MainWindow
             {
-                DataContext = new MainWindowViewModel(new PodcastService()),
+                DataContext = _mainWindowViewModel,
             };
         }
 
         base.OnFrameworkInitializationCompleted();
     }
 
+    private async void OnShutdownRequested(object? sender, ShutdownRequestedEventArgs e)
+    {
+        e.Cancel = true;
+
+        try
+        {
+            await _podcastService.SaveToDisk(_mainWindowViewModel.Podcasts);
+        }
+        catch (Exception)
+        {
+            // TODO: log the error
+        }
+        finally
+        {
+            if (ApplicationLifetime is IControlledApplicationLifetime controlledLifetime)
+            {
+                controlledLifetime.Shutdown();
+            }
+        }
+    }
+
     public override void RegisterServices()
     {
         base.RegisterServices();
 
-        AvaloniaWebViewBuilder.Initialize(default);
+        AvaloniaWebViewBuilder.Initialize(null);
     }
 }
