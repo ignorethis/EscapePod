4002905203840df1ddd597649cea56ce1ee33038
egi <daegi01@gmail.com>
2025-08-11 23:59:46 +0200

    initial result implementation

---
+++ src/EscapePod/Result.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace EscapePod;
+
+[DataContract]
+[StructLayout(LayoutKind.Auto)]
+public readonly struct Result : IEquatable<Result>
+{
+    [DataMember] private readonly bool _isOk;
+    [DataMember] private readonly string? _error;
+
+    private Result(bool isOk, string? error)
+    {
+        if (isOk && error is not null || !isOk && error is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        _isOk = isOk;
+        _error = error;
+    }
+
+    public static Result Ok()
+        => new(true, null);
+
+    public static Result Fail(string error)
+        => new(false, error);
+
+    // Convenience methods for the other Result types.
+
+    public static Result<TOk> Ok<TOk>(TOk ok)
+        => Result<TOk>.Ok(ok);
+
+    public static Result<TOk?> Fail<TOk>(string error)
+        => Result<TOk>.Fail(error);
+
+    public static Result<TOk, TError?> Ok<TOk, TError>(TOk ok)
+        => Result<TOk, TError>.Ok(ok);
+
+    public static Result<TOk?, TError> Fail<TOk, TError>(TError error)
+        => Result<TOk, TError>.Fail(error);
+
+    public bool IsOk => _isOk;
+
+    public bool IsFailure => !_isOk;
+
+    public string Error => _isOk ? throw new InvalidOperationException() : _error!;
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Result other && Equals(other);
+    }
+
+    public bool Equals(Result other)
+    {
+        return EqualityComparer<bool>.Default.Equals(_isOk, other._isOk)
+            && EqualityComparer<string>.Default.Equals(_error, other._error);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_isOk, _error);
+    }
+
+    public override string ToString()
+    {
+        return (_isOk, _error).ToString();
+    }
+
+    public static bool operator ==(Result left, Result right) =>
+        left.Equals(right);
+
+    public static bool operator !=(Result left, Result right) =>
+        !(left == right);
+}
---
+++ src/EscapePod/ResultT.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
+
+namespace EscapePod;
+
+[DataContract]
+[StructLayout(LayoutKind.Auto)]
+public readonly struct Result<TOk> : IEquatable<Result<TOk>>
+{
+    [DataMember] private readonly bool _isOk;
+    [DataMember] private readonly TOk _ok;
+    [DataMember] private readonly string? _error;
+
+    private Result(bool isOk, TOk ok, string? error)
+    {
+        if (isOk && error is not null || !isOk && error is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        _isOk = isOk;
+        _ok = ok;
+        _error = error;
+    }
+
+    public static Result<TOk> Ok(TOk ok)
+        => new(true, ok, null);
+
+    public static Result<TOk?> Fail(string? error)
+        => new(false, default, error);
+
+    public bool IsOk => _isOk;
+
+    public bool IsFailure => !_isOk;
+
+    public TOk Value => _isOk ? _ok : throw new InvalidOperationException();
+
+    public string Error => _isOk ? throw new InvalidOperationException() : _error!;
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Result<TOk> other && Equals(other);
+    }
+
+    public bool Equals(Result<TOk> other)
+    {
+        return EqualityComparer<bool>.Default.Equals(_isOk, other._isOk)
+            && EqualityComparer<TOk>.Default.Equals(_ok, other._ok)
+            && EqualityComparer<string>.Default.Equals(_error, other._error);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_isOk, _ok, _error);
+    }
+
+    public override string ToString()
+    {
+        return (_isOk, _ok, _error).ToString();
+    }
+
+    public static bool operator ==(Result<TOk> left, Result<TOk> right) =>
+        left.Equals(right);
+
+    public static bool operator !=(Result<TOk> left, Result<TOk> right) =>
+        !(left == right);
+}
